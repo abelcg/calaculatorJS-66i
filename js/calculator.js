98252021fd57bfb7e641f747ffe80120a3630ec6
abelcg <abelcg86@gmail.com>
2023-09-12 09:17:14 -0300

    add calculator styles

---
+++ js/calculator.js
@@ -0,0 +1,7 @@
+let screen = document.getElementById('screen');
+//let buttons = document.getElementsByTagName('button');
+let buttons = document.querySelectorAll('button')
+
+//console.log(screen);
+console.log(buttons);
+
